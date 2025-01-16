dc321973a17c602ed28cab732e90b25222086326
mikeuwaya <michaeluwaya@hotmail.com>
2025-01-17 00:57:29 +0100

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,15 @@ var date = document.lastModified;
 
 var year = document.lastModified;
       document.getElementById("shorter").innerHTML = " | Michael Osaregbemah Uwaya | Nigeria" + year;
+
+
+
+
+      function myFunction() {
+        var x = document.getElementById("links");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
