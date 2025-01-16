7bcb9e86b34b1060ee455eab9b1921b553e283ae
mikeuwaya <michaeluwaya@hotmail.com>
2025-01-17 00:22:16 +0100

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,6 @@
 var date = document.lastModified;
       document.getElementById("short").innerHTML = "Last modified on: " + date;
+
+
+var year = document.lastModified;
+      document.getElementById("shorter").innerHTML = " | Michael Osaregbemah Uwaya | Nigeria" + year;
