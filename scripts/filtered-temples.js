2a83761f597ee46dad7a7c6a5c9f756e88dfbfa5
mikeuwaya <michaeluwaya@hotmail.com>
2025-03-29 01:25:07 +0100

    Update filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,10 +1,142 @@
+var date = document.lastModified;
+      document.getElementById("short").innerHTML = "Last modified on: " + date;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      const cardContainer = document.getElementById('long2');
+      
+      function createCard(templeName1, location1, dedicated1, area1, imageUrl1) {
+          const card = document.createElement('div');
+          card.classList.add('card');
+      
+          const cardtempleName1 = document.createElement('h2');
+          cardtempleName1.textContent = templeName1;
+      
+          const cardlocation1 = document.createElement('p');
+          cardlocation1.textContent = location1;
+
+          const carddedicated1 = document.createElement('p');
+          carddedicated1.textContent = dedicated1;
+
+          const cardarea1 = document.createElement('p');
+          cardarea1.textContent = area1;
+
+          const cardimageUrl1 = document.createElement('p');
+          cardimageUrl1.textContent = imageUrl1;
+      
+          card.appendChild(cardtempleName1);
+          card.appendChild(cardlocation1);
+          card.appendChild(carddedicated1);
+          card.appendChild(cardarea1);
+          card.appendChild(cardimageUrl1);
+      
+          return card;
+      }
+      
+      // Example usage
+      const card1 = createCard('Aba Nigeria', 'LOCATION: Aba, Nigeria', 'DEDICATED: 2005, August, 7', 'SIZE: 11500', 'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg');
+      
+      
+      cardContainer.appendChild(card1);
+      
+      
+
+
+
+
+
+
+
+
+
+      
+const cardContainer2 = document.getElementById('long3');
+      
+function createCard(templeName2, location2, dedicated2, area2, imageUrl2) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardtempleName2 = document.createElement('h2');
+    cardtempleName2.textContent = templeName2;
+
+    const cardlocation2 = document.createElement('p');
+    cardlocation2.textContent = location2;
+
+    const carddedicated2 = document.createElement('p');
+    carddedicated2.textContent = dedicated2;
+
+    const cardarea2 = document.createElement('p');
+    cardarea2.textContent = area2;
+
+    const cardimageUrl2 = document.createElement('p');
+    cardimageUrl2.textContent = imageUrl2;
+
+    card.appendChild(cardtempleName2);
+    card.appendChild(cardlocation2);
+    card.appendChild(carddedicated2);
+    card.appendChild(cardarea2);
+    card.appendChild(cardimageUrl2);
+
+    return card;
+}
+
+// Example usage
+const card2 = createCard('Manti Utah', 'LOCATION: Aba, Nigeria', 'DEDICATED: 2005, August, 7', 'SIZE: 11500 sq ft', 'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg');
+
+
+cardContainer2.appendChild(card2);
+
+
+/* LOOP starts here*/
+var text = "templeName";
+      document.getElementById("long1").innerHTML = text;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* List of Temples*/
 const temples = [
   {
-    templeName: "Aba Nigeria",
-    location: "Aba, Nigeria",
-    dedicated: "2005, August, 7",
-    area: 11500,
-    imageUrl:
+    templeName1: "Aba Nigeria",
+    location1: "Aba, Nigeria",
+    dedicated1: "2005, August, 7",
+    area1: 11500,
+    imageUrl1:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
   {
@@ -56,55 +188,53 @@ const temples = [
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
+    templeName: "Jacksonville Florida Temple",
+    location: "Jacksonville, Florida, United States",
+    dedicated: "Announced",
+    area: N/A,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/antananarivo-madagascar-temple/400x250/antananarivo-madagascar-temple-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/jacksonville-florida-temple/400x250/jacksonville-florida-temple-wallpaper.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
+    templeName: "João Pessoa Brazil Temple",
+    location: "MexicoJoão Pessoa, Brazil",
+    dedicated: "Announced",
+    area: N/A,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/melbourne-australia-temple/400x250/melbourne-australia-temple-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/joao-pessoa-brazil-temple/400x250/joao-pessoa-brazil-temple-wallpaper.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
+    templeName: "Juchitán de Zaragoza Mexico Temple",
+    location: "Juchitán de Zaragoza, Mexico",
+    dedicated: "Announced",
+    area: N/A,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/billings-montana-temple/400x250/billings-montana-temple-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/juchitan-de-zaragoza-mexico-temple/400x250/juchitan-de-zaragoza-mexico-temple-wallpaper.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
+    templeName: "Kaohsiung Taiwan Temple",
+    location: "Kaohsiung, Taiwan",
+    dedicated: "Announced",
+    area: N/A,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/campinas-brazil-temple/400x250/campinas-brazil-temple-wallpaper.jpg"
   },  
-  // Add more temple objects here...
+  {
+    templeName: "Knoxville Tennessee Temple",
+    location: "Knoxville, Tennessee, USA",
+    dedicated: "Announced",
+    area: N/A,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/knoxville-tennessee-temple/400x250/knoxville-tennessee-temple-wallpaper.jpg"
+  },  
+  {
+    templeName: "Kyiv Ukraine Temple",
+    location: "Kyiv, Ukraine",
+    dedicated: "2010, August, 29",
+    area: 116642,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/kyiv-ukraine-temple/400x250/kyiv-ukraine-temple-wallpaper.jpg"
+  },   
 ];
 
 
-
-var date = document.lastModified;
-      document.getElementById("short").innerHTML = "Last modified on: " + date;
-
-
-var year = document.lastModified;
-      document.getElementById("shorter").innerHTML = " | Michael Osaregbemah Uwaya | Nigeria" + year;
-
-
-      function myFunction() {
-        var x = document.getElementById("links");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "block";
-        }
-      }
